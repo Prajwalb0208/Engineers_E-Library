fe71a67af8f19d7678eb7361a21cb14743f1aa29
Prajwal B <92373236+Prajwalb0208@users.noreply.github.com>
2024-06-05 21:33:16 +0530

    First Commit

--- frontend/src/api/fetchTotalBooksCount.js
+++ frontend/src/api/fetchTotalBooksCount.js
@@ -7,7 +7,7 @@ const TOTAL_BOOKS_TO_FETCH = 3200;
 const fetchTotalBooksCount = async () => {
   try {
     const allBooks = await fetchBooks(1); // Fetch books for the first page
-    const totalBooksCount = allBooks.numFound || 3200; // Extract total count from API response
+    const totalBooksCount = allBooks.numFound || 800; // Extract total count from API response
     return Math.min(totalBooksCount, TOTAL_BOOKS_TO_FETCH); // Return the minimum of actual count and the limit
   } catch (error) {
     console.error('Error fetching total books count:', error);
