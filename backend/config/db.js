5be27b2f1392e315f42fe722a183a7d8aabb31a3
Prajwal B <92373236+Prajwalb0208@users.noreply.github.com>
2024-07-21 19:26:36 +0530

    Final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 export const connectDB = async() => {
     await mongoose.connect('mongodb+srv://engineerselibrary:eolfk6I66eVBZxZg@cluster0.urvg2l2.mongodb.net/e-library').then(()=>console.log("DB connected"));
 }
